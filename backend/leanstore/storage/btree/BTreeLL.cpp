68a02a593c4130d6e2213872eb6351be376bfc49
Driramohamedfarouk <mohamedfarouk.drira@insat.ucar.tn>
2025-12-15 19:27:15 +0000

    make the single version BTree work as expected

--- backend/leanstore/storage/btree/BTreeLL.cpp
+++ backend/leanstore/storage/btree/BTreeLL.cpp
@@ -169,9 +169,8 @@ OP_RESULT BTreeLL::insert(u8* o_key, u16 o_key_length, u8* o_value, u16 o_value_
          std::memcpy(wal_entry->payload, key.data(), key.length());
          std::memcpy(wal_entry->payload + key.length(), value.data(), value.length());
          wal_entry.submit();
-      } else {
-         iterator.markAsDirty();
       }
+      iterator.markAsDirty();
       jumpmu_return OP_RESULT::OK;
    }
    jumpmuCatch() {}
@@ -397,8 +396,8 @@ OP_RESULT BTreeLL::updateSameSizeInPlace(u8* o_key,
          wal_entry.submit();
       } else {
          callback(current_value.data(), current_value.length());
-         iterator.markAsDirty();
       }
+      iterator.markAsDirty();
       iterator.contentionSplit();
       jumpmu_return OP_RESULT::OK;
    }
@@ -430,10 +429,8 @@ OP_RESULT BTreeLL::remove(u8* o_key, u16 o_key_length)
          std::memcpy(wal_entry->payload, key.data(), key.length());
          std::memcpy(wal_entry->payload + o_key_length, value.data(), value.length());
          wal_entry.submit();
-         iterator.markAsDirty();
-      } else {
-         iterator.markAsDirty();
       }
+      iterator.markAsDirty();
       ret = iterator.removeCurrent();
       ensure(ret == OP_RESULT::OK);
       iterator.mergeIfNeeded();
